73e442bbfe1ecb81a66af678ac04af0e014a94e8
cacheUp <bradb26@gmail.com>
2019-08-26 15:57:32 +0300

    bug fix: invoke Enzyme Adapter

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Enzyme, {shallow} from "enzyme"
-import EnzymeAdapter from "enzyme-adapter-react-16"
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-Enzyme.configure({adapter: new EnzymeAdapter})
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders without crashing', () => {
- const wrapper = shallow(<App/>)
- 
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
 });
