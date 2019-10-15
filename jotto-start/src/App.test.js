df9895580737c711ad616a3ebac023a8d12dc0a9
cacheUp <bradb26@gmail.com>
2019-10-15 11:11:55 +0300

    joto init

---
+++ jotto-start/src/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
