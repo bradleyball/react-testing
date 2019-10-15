693a22d318229b678600861d567d24206ad9b885
cacheUp <bradb26@gmail.com>
2019-10-15 15:49:07 +0300

    added check props test

--- jotto-start/src/GuessedWords.test.js
+++ jotto-start/src/GuessedWords.test.js
@@ -8,3 +8,19 @@ const defaultProps = [
     guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }]
   }
 ];
+
+/**
+ * Factory function to create a ShallowWrap for the GuessedWords component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}  returns the shallow wrapper
+ */
+
+const setup = (props = {}) => {
+  setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
