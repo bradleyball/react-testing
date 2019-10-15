5301823d172879142e668e480f3470010799993c
cacheUp <bradb26@gmail.com>
2019-10-15 11:49:53 +0300

    findByAttr with js docs

--- jotto-start/src/Congrats.test.js
+++ jotto-start/src/Congrats.test.js
@@ -2,8 +2,16 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import Congrats from "./Congrats";
+import { findByTestAttr } from "../test/testUtils";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrap for the Congrats component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}  returns the shallow wrapper
+ */
 
 const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
