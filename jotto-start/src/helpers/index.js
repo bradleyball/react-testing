646d0ba1adf7fb960c25a51b1a4389ea70d9dcd8
cacheUp <bradb26@gmail.com>
2019-10-15 17:06:35 +0300

    all tests pass

--- jotto-start/src/helpers/index.js
+++ jotto-start/src/helpers/index.js
@@ -5,4 +5,9 @@
  * @returns {number} Number of letters matched between guessed word and secret word
  */
 
-export function getLetterMatchCount(guessedWord, secretWord) {}
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetterSet = new Set(secretWord.split(""));
+  const guessedLetterSet = new Set(guessedWord.split(""));
+  return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
+    .length;
+}
