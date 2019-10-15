6338f3b59e6742ebc2d054e316033e6596ccdfaa
cacheUp <bradb26@gmail.com>
2019-10-15 11:57:55 +0300

    failing test written

--- jotto-start/test/testUtils.js
+++ jotto-start/test/testUtils.js
@@ -1,10 +1,10 @@
 /**
  * Return node(s) with the given data-test attribute
  * @param {ShallowWrapper} wrapper  Enzyme shallow wrapper
- * @param {*} val Value of data-test attribute for search
+ * @param {string} val Value of data-test attribute for search
  * @returns {ShallowWrapper}
  */
 
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test=${val}]`);
+  return wrapper.find(`[data-test="${val}"]`);
 };
