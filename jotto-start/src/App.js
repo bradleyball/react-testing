7eb74ba514438238e4836c3cc1975ebb08dcbc02
cacheUp <bradb26@gmail.com>
2019-10-15 16:44:25 +0300

    change app to class

--- jotto-start/src/App.js
+++ jotto-start/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function App() {
-  return <div className="App">hey</div>;
+class App extends React.Component {
+  render() {
+    return <div className="App">hey</div>;
+  }
 }
 
 export default App;
