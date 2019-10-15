41e75506804166db1cd2cf333fd194bcebe13906
cacheUp <bradb26@gmail.com>
2019-10-15 16:15:57 +0300

    test passed

--- jotto-start/src/GuessedWords.js
+++ jotto-start/src/GuessedWords.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = props => {
-  return <div></div>;
+  return <div data-test="component-guessed-words"></div>;
 };
 
 GuessedWords.propTypes = {
