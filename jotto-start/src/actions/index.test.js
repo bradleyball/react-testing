758c8903910a5b0822e4e3bf1f202ecc437e0b4c
cacheUp <bradb26@gmail.com>
2019-10-16 14:33:41 +0300

    js doc

--- jotto-start/src/actions/index.test.js
+++ jotto-start/src/actions/index.test.js
@@ -2,6 +2,11 @@ export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS"
 };
 
+/**
+ * @function correctGuess
+ * @returns {object} Action object with type "CORRECT_GUESS"
+ */
+
 export function correctGuess() {
   return {};
 }
