6d1ba79f853b7af35208c0e1e9cb1ab3750962f2
cacheUp <bradb26@gmail.com>
2019-10-16 14:57:39 +0300

    Input init

---
+++ jotto-start/src/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { conenct } from "react-redux";
+import { connect } from "net";
+
+class Input extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);
