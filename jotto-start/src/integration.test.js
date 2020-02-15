07143bbf9fffb9ac9c91e926d72995c1d38cdca7
cacheUp <bradb26@gmail.com>
2020-02-15 15:11:14 +0300

    update

--- jotto-start/src/integration.test.js
+++ jotto-start/src/integration.test.js
@@ -2,8 +2,30 @@ import { storeFactory } from "../test/testUtils";
 import { guessWord } from "./actions";
 
 describe("guessWord action dispatcher", () => {
+  const secretWord = "party";
+  const unsuccessfulGuess = "train";
   describe("no guessed words", () => {
-    test("updates state correctly for unsuccessful guess", () => {});
+    let store;
+    const initalState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initalState);
+    });
+    test("updates state correctly for unsuccessful guess", () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initalState,
+        success: false,
+        guessedWords: [
+          {
+            guessedWord: unsuccessfulGuess,
+            letterMatchCount: 3
+          }
+        ]
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
 
     test("updates state correctly for unsuccessful guess", () => {});
   });
